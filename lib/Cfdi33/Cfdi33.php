0de5f1b88d9096c2686911eb5fdf6bad7cb8ec65
Sergio Rivera <nokiacorporation1@gmail.com>
2021-12-14 17:18:28 -0600

    Added functions to get verification URL and TFD original string

--- lib/Cfdi33/Cfdi33.php
+++ lib/Cfdi33/Cfdi33.php
@@ -6,6 +6,8 @@ use Lib\Cfdi33\Comprobante33;
 
 class Cfdi33
 {
+    const VERIFY_CFDI_URL = 'https://verificacfdi.facturaelectronica.sat.gob.mx/default.aspx';
+    const TFD_11_TEMPLATE = '||**Version**|**UUID**|**FechaTimbrado**|**RfcProvCertif**|**SelloCFD**|**NoCertificadoSAT**|||';
 
     /**
      * Return new empty Comprobante33 object
@@ -36,5 +38,46 @@ class Cfdi33
         } catch (\Exception $e) {
             return null;
         }
+    }
+
+    /**
+     * Generates tfc original string from xml path
+     * 
+     * @param $xmlFile
+     * @return string
+     */
+    public static function generateTfdOriginalString($xmlFile)
+    {
+        $cfdiData = self::xmlToJson(file_get_contents($xmlFile));
+        $tfd = $cfdiData->Complemento->TimbreFiscalDigital ?? false;
+
+        if (!$tfd) return '';
+
+        $tfdString = self::TFD_11_TEMPLATE;
+        $tfdString = str_replace('**Version**', $tfd->Version, $tfdString);
+        $tfdString = str_replace('**UUID**', $tfd->UUID, $tfdString);
+        $tfdString = str_replace('**FechaTimbrado**', $tfd->FechaTimbrado, $tfdString);
+        $tfdString = str_replace('**RfcProvCertif**', $tfd->RfcProvCertif, $tfdString);
+        $tfdString = str_replace('**SelloCFD**', $tfd->SelloCFD, $tfdString);
+        $tfdString = str_replace('**NoCertificadoSAT**|', $tfd->NoCertificadoSAT, $tfdString);
+
+        return $tfdString;
+    }
+
+    /**
+     * Returns verification url given xml path
+     * 
+     * @param $xmlFile
+     * @return string
+     */
+    public static function generateVerificationUrl($xmlFile)
+    {
+        $cfdiData = self::xmlToJson(file_get_contents($xmlFile));
+        $verifyUrl = self::VERIFY_CFDI_URL;
+        $verifyUrl = $verifyUrl . '?id=' . $cfdiData->Complemento->TimbreFiscalDigital->UUID;
+        $verifyUrl = $verifyUrl . '&re=' . $cfdiData->Emisor->Rfc;
+        $verifyUrl = $verifyUrl . '&rr=' . $cfdiData->Receptor->Rfc;
+        $verifyUrl = $verifyUrl . '&tt=' . $cfdiData->Total;
+        return $verifyUrl . '&fe=' . substr($cfdiData->Complemento->TimbreFiscalDigital->SelloCFD, -8);
     }
 }
---
+++ tests/CadenaOriginalTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Test;
+
+require_once __DIR__ . "../../vendor/autoload.php";
+use PHPUnit\Framework\TestCase;
+
+use Lib\Cfdi33\Cfdi33;
+use Lib\Cfdi33\Xml33;
+use Lib\Cfdi40\Cfdi40;
+
+class CadenaOriginalTest extends TestCase
+{
+    const FILES_TO_TEST = [
+        "assets/33/facturacion_normal.xml",
+        "assets/33/complemento_pago.xml",
+        "assets/33/nota_credito.xml",
+        "assets/33/sustitucion.xml"
+    ];
+    
+    public function testTfdOriginalString()
+    {
+        echo "\n *------ Testing tfd original string ------* \n";
+        foreach (self::FILES_TO_TEST as $file) {
+            $result = Cfdi33::generateTfdOriginalString($file);
+            echo "------ File: $file ------ \n";
+            echo json_encode($result, JSON_PRETTY_PRINT) . "\n";
+            $this->assertNotNull($result);    
+        }
+    }
+
+    public function testVerifyUrl()
+    {
+        echo "\n *------ Testing verify URL ------* \n";
+        foreach (self::FILES_TO_TEST as $file) {
+            $result = Cfdi33::generateVerificationUrl($file);
+            echo "------ File: $file ------ \n";
+            echo json_encode($result, JSON_PRETTY_PRINT) . "\n";
+            $this->assertNotNull($result);
+        }
+    }
+}
